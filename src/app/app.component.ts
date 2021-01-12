8a14c788e93c6b5d7e8cffd6fd2057a8cf3d55b4
Denis <dnsmsv@gmail.com>
2021-01-12 16:05:27 +0300

    Catalog can always be opened

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CatalogService } from './services/catalog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private catalogService: CatalogService) {}
+
   title = 'shop';
+
+  hideCatalog(): void {
+    this.catalogService.hideCatalog();
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,23 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CatalogService } from '../services/catalog.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-  constructor(private catalogService: CatalogService) {}
-
-  catalogVisibility: boolean = false;
-
-  ngOnInit(): void {
-    this.catalogService.catalogVisibility.subscribe((visibility) => {
-      this.catalogVisibility = visibility;
-    });
-  }
-
-  hideCatalog(): void {
-    this.catalogService.hideCatalog();
-  }
-}
+export class MainComponent {}
